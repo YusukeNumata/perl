07a37dbea9883f55adb282b000b92c0d7613907b
YusukeNumata <numatayuusuke@MBA-YNumata.local>
2013-11-03 18:27:52 +0900

    Day06 ランダム
    処理を関数化

--- Day06/RandomSummary.pl
+++ Day06/RandomSummary.pl
@@ -1,7 +1,11 @@
+samarry();
+samarry();
 
-for my $i (1 .. 10) {
-  $int1 = int(rand(10)) + 1;
-  $int2 = int(rand(10)) + 1;
-  print $int1, "+", $int2, "=", $int1 + $int2,  "\n";
+sub samarry {
+  for my $i (1 .. 10) {
+    $int1 = int(rand(10)) + 1;
+    $int2 = int(rand(10)) + 1;
+    print $int1, "+", $int2, "=", $int1 + $int2,  "\n";
+  }
 }
 
